b474907504e2ea16066b34e448f935904fc98094
SamuelSvahn <samuelsvahn@hotmail.com>
2023-01-10 14:46:31 +0100

    Cleaned the code now will start with logics

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -14,5 +14,6 @@ public class App
 
         StudentDAO studentDAO = context.getBean(StudentDAO.class);
 
+        ComponentScanConfig componentScanConfig= context.getBean(ComponentScanConfig.class);
     }
 }
---
+++ src/main/java/se/lexicon/Service/StudentManagementImpl.java
@@ -0,0 +1,53 @@
+package se.lexicon.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Component;
+import se.lexicon.config.ComponentScanConfig;
+import se.lexicon.data_access.StudentDAO;
+import se.lexicon.Service.StudentManagement;
+import se.lexicon.models.Student;
+
+import java.util.List;
+@Component
+public class StudentManagementImpl implements StudentManagement {
+
+    @Autowired
+    StudentDAO studentDAO;
+    @Autowired
+    ComponentScanConfig scanner;
+
+
+
+
+
+    @Override
+    public Student create() {
+        return null;
+    }
+
+    @Override
+    public Student save(Student student) {
+        return null;
+    }
+
+    @Override
+    public Student find(int id) {
+        return null;
+    }
+
+    @Override
+    public Student remove(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return null;
+    }
+
+    @Override
+    public Student edit(Student student) {
+        return null;
+    }
+}
